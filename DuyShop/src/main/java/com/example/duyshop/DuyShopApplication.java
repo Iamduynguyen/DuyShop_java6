fe5f34a2544179b801ef5f9d0248bd4da0adebb6
Iamduynguyen <81656432+Iamduynguyen@users.noreply.github.com>
2021-10-28 16:12:16 +0700

    create by duynguyen

---
+++ DuyShop/src/main/java/com/example/duyshop/DuyShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.duyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuyShopApplication.class, args);
+    }
+
+}
---
+++ DuyShop/src/test/java/com/example/duyshop/DuyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuyShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
